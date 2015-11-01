fb60c04d80f6dfdf59e9ac573e9b469e10f97963
nanstc <nanstchen@gmail.com>
2015-10-31 21:01:46 -0700

    geocoding

---
+++ InsTrail/app/controllers/geocoding_controller.rb
@@ -0,0 +1,9 @@
+class GeocodingController < ApplicationController
+  =begin
+  def get_coordiantes
+    do |trailName|
+    @lat_long = Geocoder.coordinates("Eiffle Tower")
+    puts @lat_long 
+  end
+  =end
+end
