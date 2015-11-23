0f08517b4efb9f38e2056b6c6da9a1200c4d93cf
elizabethhu <elizabethhu3@gmail.com>
2015-11-22 23:04:10 -0800

    More settings changes

--- InsTrail/app/controllers/settings_controller.rb
+++ InsTrail/app/controllers/settings_controller.rb
@@ -1,12 +1,16 @@
 class SettingsController < ApplicationController
-  def index
-  end
-  
-  def set_settings
-    
-    if request.post? 
+  @setting
 
-        
+  def index
+  	#@user = User.where(:nickname => 'semisezer')
+  	@setting = Setting.create
+  	@setting.number = 5
+  	
+  end
+
+  def set_settings
+    @setting.popularity = 10
+    if request.post? 
 
     end
     
@@ -14,18 +18,24 @@ class SettingsController < ApplicationController
     puts setting
     
   end
-  
-  def create
-    puts 'CREATE CALLED'
-    @popularity = set_settings(popularity_params) 
-    if @popularity.save 
-      redirect_to '/settings' 
-    else 
-      puts 'else'
-    end
+
+
+  def update
+  	puts 'UPDATE CALLED!'
+  	puts setting_params[:number]
+  	number = setting_params[:number]
+  	puts 'SETTING DOWN HERE!'
+  	puts @setting
+  	#@setting.update_attribute(:number, number)
+  	#puts @setting[:number]
+  	flash[:success] = "Popularity updated to: #{number}"
+  	redirect_to "/settings/"
+
+  	#@setting.number = setting_params
   end
-  
-    def popularity_params
-       params.require(:number)
-    end
+
+  private
+  def setting_params
+  	params.require(:setting)
+  end
 end
