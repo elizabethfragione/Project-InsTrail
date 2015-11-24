76c2633af1cbaf5402f463a57de7472e50b70959
semihsezer <semih.c.sezer@gmail.com>
2015-11-24 15:39:50 -0800

    Heroku authentication fix - history loads but not correctly

--- InsTrail/config/initializers/omniauth.rb
+++ InsTrail/config/initializers/omniauth.rb
@@ -1,3 +1,13 @@
+#Rails.application.config.middleware.use OmniAuth::Builder do
+	#provider :instagram, '49316a6ab7b0475a8c45802e02a19143', 'f39add31dd9c425f906aded8aba07800'
+#end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :instagram, '49316a6ab7b0475a8c45802e02a19143', 'f39add31dd9c425f906aded8aba07800'
+	provider :instagram, '49316a6ab7b0475a8c45802e02a19143', 'f39add31dd9c425f906aded8aba07800', {
+	  client_options: {ssl: {
+	    ca_file: 'usr/lib/ssl/certs/ca-certificates.crt',
+      ca_path: '/etc/ssl/certs'
+	  }
+  }
+}
 end
