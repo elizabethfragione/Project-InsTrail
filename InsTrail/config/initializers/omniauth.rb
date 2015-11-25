6c9ee3efe6b40af0698625f437d28aab77d22aa5
nanstc <nanstchen@gmail.com>
2015-11-24 17:26:17 -0800

    new bars for setting and used user_id to identify trails

--- InsTrail/config/initializers/omniauth.rb
+++ InsTrail/config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
-#Rails.application.config.middleware.use OmniAuth::Builder do
-	#provider :instagram, '49316a6ab7b0475a8c45802e02a19143', 'f39add31dd9c425f906aded8aba07800'
-#end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :instagram, '49316a6ab7b0475a8c45802e02a19143', 'f39add31dd9c425f906aded8aba07800', {
 	  client_options: {ssl: {
