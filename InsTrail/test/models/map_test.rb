bbc2487ced0dd06620bb31f64408284276833f13
elizabethhu <elizabethhu3@gmail.com>
2015-11-25 14:37:59 -0800

    tests

--- InsTrail/test/models/map_test.rb
+++ InsTrail/test/models/map_test.rb
@@ -1,4 +1,15 @@
 require 'test_helper'
 
 class MapTest < ActiveSupport::TestCase
+  
+  test "point is in vancouver" do
+    lat_lon = Geocoder.coordinates("Lynn Canyon Park")
+    assert in_vancouver(lat_lon), "point is in Vancouver"
+  end
+  
+  test "point is NOT in vancouver" do
+    lat_lon = Geocoder.coordinates("Cable Bay Trail")
+    assert in_vancouver(lat_lon), "point is NOT in Vancouver"
+  end
+  
 end
